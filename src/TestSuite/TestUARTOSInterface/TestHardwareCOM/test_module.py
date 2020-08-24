666fbac0c1a84593db642b24a9a49555d4d3a5c6
nulltek <steve.public@nulltek.xyz>
2020-08-24 12:41:25 +1200

    Completed test cases for static NPC methods in the abstraction layer.

--- src/TestSuite/TestUARTOSInterface/TestHardwareCOM/test_module.py
+++ src/TestSuite/TestUARTOSInterface/TestHardwareCOM/test_module.py
@@ -15,13 +15,60 @@ class TestHardwareCOMInterface:
 
 
 class TestHardwareCOMAbstractions:
+    TEST_PACKETS = [
+        {"addr_to": 0,
+         "addr_from": 1,
+         "payload": [1],
+         "checksum": 253,
+         "binary": b">\x00\x01\x01\x01\xfd<"
+         },
+        {"addr_to": 64,
+         "addr_from": 0,
+         "payload": [13, 0, 1, 12, 1, 0],
+         "checksum": 159,
+         "binary": b">\x40\x00\x06\x0d\x00\x01\x0c\x01\x00\x9f<"
+         },
+        # Bad packet
+        {"addr_to": 256,
+         "addr_from": 256,
+         "payload": [],
+         "checksum": 0,
+         "binary": b""}
+    ]
 
     # Checks the static function correctly computes the LRC checksums for some known packets.
-    @pytest.mark.parametrize("test_packet_data, expected_lrc", [
-                                [[255], 1],  # overflow case
-                                [[0], 0],  # base case
-                                [[0, 1, 1, 1], 253],  # simple NPC packet case
-                                [[254, 0, 3, 255, 0, 13], 243]  # typical NPC packet case
-                             ])
+    @pytest.mark.parametrize(
+        "test_packet_data, expected_lrc",
+        [
+            [[255], 1],  # overflow case
+            [[0], 0],  # base case
+            [(  # simple NPC packet case
+                [TEST_PACKETS[0]["addr_to"], TEST_PACKETS[0]["addr_from"],
+                 len(TEST_PACKETS[0]["payload"])] + TEST_PACKETS[0]["payload"]
+            ), TEST_PACKETS[0]["checksum"]],
+            [(  # simple NPC packet case
+                [TEST_PACKETS[1]["addr_to"], TEST_PACKETS[1]["addr_from"],
+                 len(TEST_PACKETS[1]["payload"])] + TEST_PACKETS[1]["payload"]
+            ), TEST_PACKETS[1]["checksum"]],
+            [(  # simple NPC packet case
+                    [TEST_PACKETS[2]["addr_to"], TEST_PACKETS[2]["addr_from"],
+                     len(TEST_PACKETS[2]["payload"])] + TEST_PACKETS[2]["payload"]
+            ), TEST_PACKETS[2]["checksum"]],
+        ]
+    )
     def test_get_npc_checksum(self, test_packet_data: [], expected_lrc: int):
+        print(f"packet: {test_packet_data}, lrc:{expected_lrc}")
         assert UOSInterface.UOSInterface.get_npc_checksum(test_packet_data) == expected_lrc
+
+    # Checks the static function correctly forms packets using some known examples
+    @pytest.mark.parametrize("test_packet", [TEST_PACKETS[0], TEST_PACKETS[1], TEST_PACKETS[2]])
+    def test_get_npc_packet(self, test_packet: {}):
+        print(
+            f"addr_to: {test_packet['addr_to']}, addr_from: {test_packet['addr_from']}, "
+            f"payload: {test_packet['payload']}, packet: {test_packet['binary']}"
+        )
+        assert UOSInterface.UOSInterface.get_npc_packet(
+            test_packet["addr_to"],
+            test_packet["addr_from"],
+            test_packet["payload"]
+        ) == test_packet["binary"]
--- src/UARTOSInterface/HardwareCOM/UOSInterface.py
+++ src/UARTOSInterface/HardwareCOM/UOSInterface.py
@@ -13,7 +13,7 @@ class UOSInterface(metaclass=ABCMeta):
     @staticmethod
     def get_npc_packet(to_addr: int, from_addr: int, payload: [int]) -> bytes:
         if to_addr < 256 and from_addr < 256 and len(payload) < 256:  # check input is possible to parse
-            packet_data = [to_addr, from_addr] + payload
+            packet_data = [to_addr, from_addr, len(payload)] + payload
             lrc = UOSInterface.get_npc_checksum(packet_data)
             return bytes([0x3e, packet_data[0], packet_data[1], len(payload)] + payload + [lrc, 0x3c])
         return bytes([])
