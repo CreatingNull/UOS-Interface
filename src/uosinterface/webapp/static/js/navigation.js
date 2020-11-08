48444fe322d09f90eabf1bc16640c54224c2c1fe
nulltek <steve.public@nulltek.xyz>
2020-11-08 15:37:37 +1300

    Persisted user device selection on front-end.

--- src/uosinterface/webapp/static/js/navigation.js
+++ src/uosinterface/webapp/static/js/navigation.js
@@ -3,9 +3,14 @@ window.onscroll = function () {
   stickNavbar();
 };
 
+// When the page loads load device connection if persisted.
+window.onload = function () {
+  getPersistedDeviceSelection();
+};
+
 // Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon.
 function dynamicNavigation() {
-  var nav = document.getElementById("top-navigation");
+  let nav = document.getElementById("top-navigation");
   if (nav.classList.contains("responsive")) {
     nav.classList.remove("responsive");
   } else {
@@ -15,8 +20,8 @@ function dynamicNavigation() {
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position.
 function stickNavbar() {
-  var navbar = document.getElementById("top-navigation"); // Get the navbar
-  var stickyHeight = navbar.offsetTop; // Get the offset position of the navbar
+  let navbar = document.getElementById("top-navigation"); // Get the navbar
+  let stickyHeight = navbar.offsetTop; // Get the offset position of the navbar
   if (window.pageYOffset > stickyHeight) {
     navbar.classList.add("sticky");
   } else {
@@ -26,7 +31,32 @@ function stickNavbar() {
 
 // takes the currently selected navbar connection and writes this as the device connection form field.
 function getSelectedConnection() {
-  var sel = document.getElementById("device-select");
+  let sel = document.getElementById("device-select");
   document.getElementById("form-device-connection").value =
     sel.options[sel.selectedIndex].value;
+}
+
+// Uses javascript local storage to ensure device selection in nonvolatile over the session.
+function persistDeviceSelection() {
+  let sel = document.getElementById("device-select");
+  window.localStorage.setItem(
+    "device-selection",
+    sel.options[sel.selectedIndex].value
+  );
+}
+
+// Returns the previous device selection if it has been persisted.
+function getPersistedDeviceSelection() {
+  if (window.localStorage.getItem("device-selection") !== null) {
+    let sel = document.getElementById("device-select");
+    for (let i = 0; i < sel.options.length; i++) {
+      if (
+        sel.options[i].value === window.localStorage.getItem("device-selection")
+      ) {
+        sel.options[i].setAttribute("selected", "");
+      } else if (sel.options[i].hasAttribute("selected")) {
+        sel.options[i].removeAttribute("selected");
+      }
+    }
+  }
 }
