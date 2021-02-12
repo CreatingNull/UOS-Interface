463a60f0413e6195cfbfd09db946616049235eae
nulltek <steve.public@nulltek.xyz>
2021-02-12 19:09:22 +1300

    Updated systems test to produce coverage report.

--- resources/jenkins_systems_test.sh
+++ resources/jenkins_systems_test.sh
@@ -8,6 +8,8 @@ ci_python=venv/bin/python
 $ci_python -m pip install --upgrade pip
 $ci_python -m pip install --upgrade setuptools
 $ci_python -m pip install -r resources/requirements.txt
+$ci_python -m pip install wheel
 $ci_python -m pip install pytest
 $ci_python -m pip install coverage
-$ci_python -m pytest src/tests/ --usb-serial=/dev/ttyUSB0
+$ci_python -m coverage run --rc-file=coveragerc.ini -m pytest src/tests/ --usb-serial=/dev/ttyUSB0
+$ci_python -m coverage xml --rc-file=coveragerc.ini
