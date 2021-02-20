8c0e0fdff134376d75f006cd41ba55bb9e748d3b
nulltek <steve.public@nulltek.xyz>
2021-02-20 17:55:11 +1300

    Fixed the sha-bang. Added coverage result uploading if API token env variable exists.

--- resources/jenkins_systems_test.sh
+++ resources/jenkins_systems_test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Shell script for configuring the UOS-Jenkins system's test rack.
 # This is for testing coverage of hardware layers and interfaces.
@@ -14,4 +14,8 @@ $ci_python -m pip install coverage
 $ci_python -m coverage run --rcfile=.coveragerc.ini -m pytest src/tests/ --usb-serial=/dev/ttyUSB0
 $ci_python -m coverage xml --rcfile=.coveragerc.ini
 
-# After this coverage report is uploaded to codacy project against the commit hash.
+# After this coverage report is uploaded to codacy project against the commit hash
+# API Token must already be injected as protected environment variable.
+if [[ -v ${COMMIT_ID} ]]; then
+  <(curl -Ls https://coverage.codacy.com/get.sh) report --commit-uuid "$COMMIT_ID" -r ../logs/coverage/coverage.xml.
+fi
