50c29cea0a2ff6db8758c845911455ff5d9ca37d
nulltek <steve.public@nulltek.xyz>
2021-03-23 11:06:13 +1300

    Adjusted pin config to reflect available functionality on pins.

--- src/uosinterface/hardware/config.py
+++ src/uosinterface/hardware/config.py
@@ -9,14 +9,33 @@ INTERFACE_STUB = "STUB"
 
 
 @dataclass
+class Pin:
+    """Defines supported features of the pin."""
+
+    gpio_out: bool = False
+    gpio_in: bool = False
+    dac_out: bool = False
+    pwm_out: bool = False
+    adc_in: bool = False
+    pull_up: bool = False
+    pull_down: bool = False
+    pc_int: bool = False
+    hw_int: bool = False
+    timer: {} = field(default_factory=dict)
+    comp: {} = field(default_factory=dict)
+    spi: {} = field(default_factory=dict)
+    i2c: {} = field(default_factory=dict)
+
+
+@dataclass
 class Device:
     """Define an implemented UOS device dictionary."""
 
     name: str
     interfaces: list
     functions_enabled: Dict
-    digital_pins: List = field(default_factory=list)
-    analogue_pins: List = field(default_factory=list)
+    digital_pins: {int: Pin} = field(default_factory=dict)
+    analogue_pins: Dict = field(default_factory=dict)
     aux_params: Dict = field(default_factory=dict)
 
 
@@ -72,8 +91,103 @@ ARDUINO_NANO_3 = Device(
         "get_system_info": {0: True},
         "get_gpio_config": {0: True},
     },
-    digital_pins=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    analogue_pins=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+    digital_pins={
+        2: Pin(gpio_out=True, gpio_in=True, pull_up=True, pc_int=True, hw_int=True),
+        3: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pwm_out=True,
+            pc_int=True,
+            hw_int=True,
+        ),
+        4: Pin(gpio_out=True, gpio_in=True, pull_up=True, pc_int=True),
+        5: Pin(gpio_out=True, gpio_in=True, pull_up=True, pwm_out=True, pc_int=True),
+        6: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pwm_out=True,
+            pc_int=True,
+            comp={"type": "low", "bus": 0},
+        ),
+        7: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pc_int=True,
+            comp={"type": "high", "bus": 0},
+        ),
+        8: Pin(gpio_out=True, gpio_in=True, pull_up=True, pc_int=True),
+        9: Pin(gpio_out=True, gpio_in=True, pull_up=True, pwm_out=True, pc_int=True),
+        10: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pwm_out=True,
+            pc_int=True,
+            spi={"type": "ss", "bus": 0},
+        ),
+        11: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pwm_out=True,
+            pc_int=True,
+            spi={"type": "mosi", "bus": 0},
+        ),
+        12: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pc_int=True,
+            spi={"type": "miso", "bus": 0},
+        ),
+        13: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pc_int=True,
+            i2c={"type": "sck", "bus": 0},
+        ),
+        14: Pin(
+            gpio_out=True, gpio_in=True, pull_up=True, pc_int=True
+        ),  # analogue pin 0
+        15: Pin(
+            gpio_out=True, gpio_in=True, pull_up=True, pc_int=True
+        ),  # analogue pin 1
+        16: Pin(
+            gpio_out=True, gpio_in=True, pull_up=True, pc_int=True
+        ),  # analogue pin 2
+        17: Pin(
+            gpio_out=True, gpio_in=True, pull_up=True, pc_int=True
+        ),  # analogue pin 3
+        18: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pc_int=True,
+            i2c={"type": "sda", "bus": 0},
+        ),  # analogue pin 4
+        19: Pin(
+            gpio_out=True,
+            gpio_in=True,
+            pull_up=True,
+            pc_int=True,
+            i2c={"type": "scl", "bus": 0},
+        ),  # analogue pin 5
+    },
+    analogue_pins={
+        0: Pin(adc_in=True),
+        1: Pin(adc_in=True),
+        2: Pin(adc_in=True),
+        3: Pin(adc_in=True),
+        4: Pin(adc_in=True),
+        5: Pin(adc_in=True),
+        6: Pin(adc_in=True),
+        7: Pin(adc_in=True),
+        8: Pin(adc_in=True),
+    },
     aux_params={"default_baudrate": 115200},
 )
 
